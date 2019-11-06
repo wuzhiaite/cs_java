79887d71efc8c960055d46dff0bc1ef946bdae3a
Pengfei Li <lipengfei@cmbi.com.hk>
2019-11-06 17:43:44 +0800

    SQL单表查询拼接方法

--- src/main/java/com/wuzhiaite/javaweb/module/pagelistconfig/service/check/SelectParamCheck.java
+++ src/main/java/com/wuzhiaite/javaweb/module/pagelistconfig/service/check/SelectParamCheck.java
@@ -1,15 +1,21 @@
 package com.wuzhiaite.javaweb.module.pagelistconfig.service.check;
 
 import com.wuzhiaite.javaweb.module.pagelistconfig.entity.SelectField;
+import org.springframework.util.Assert;
+import org.thymeleaf.util.StringUtils;
 
+/**
+ * select参数校验
+ */
 public class SelectParamCheck implements ParamCheckInterface<SelectField>{
 
+    private String regex = "";
 
     @Override
     public boolean checkParam(SelectField selectField) {
-
-
-
-        return false;
+        if(StringUtils.isEmpty(selectField.getFiled())) return false;
+        if(StringUtils.isEmpty(selectField.getAlias())) selectField.setAlias(selectField.getFiled());
+        if(selectField.getType() == null ) selectField.setType(SelectField.SelectEnum.DEFAULT);
+        return true;
     }
 }
