2ef833bbe950c73028951d4fe98b74598aecbb98
wuzhiaite <656494298@qq.com>
2020-08-28 18:07:01 +0800

    增加了七牛存储管理

---
+++ src/main/java/com/wuzhiaite/javaweb/base/qiniu/VariableName.java
@@ -0,0 +1,19 @@
+package com.wuzhiaite.javaweb.base.qiniu;
+/**
+ *   类作用描述：命名类
+ *   创建时间：2018/12/1 17:54
+ *   构造方法参数：
+ *   修改时间：2018/12/1 17:54
+ *   创建者：ZENG
+ *   类全限定名称：com.hyxiaojingyu.common.VariableName
+ **/
+public class VariableName {
+    //    七牛AK
+    public static final String accessKey = "vAb_HJfQjjk5eB67CxCsGdM-6mQUHq86idaDk9qU";
+    //    七牛SK
+    public static final String secretKey = "JOq9eyH1SQYAX6EcfBO-gsGbBBTs0vLiJypj6YkH";
+    //    七牛存储空间名
+    public static final String bucket = "wuzhiaite";
+    //    七牛默认域名
+    public static final String domain = "http://qfrpxei77.hn-bkt.clouddn.com";
+}
---
+++ src/main/java/com/wuzhiaite/javaweb/common/monitor/controller/LogController.java
@@ -0,0 +1,26 @@
+package com.wuzhiaite.javaweb.common.monitor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 实时读取服务器的日志文件
+ */
+@Controller
+@RequestMapping("/logs/")
+public class LogController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
